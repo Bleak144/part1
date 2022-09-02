f0cdfbe94a75f8a675cf230b42b0af0e76f776f1
Bleakness <arcadegames144@gmail.com>
2022-09-02 18:45:37 -0500

    Unicafe, CourseInfo, Anecdotes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { useState } from "react";
 
 
 const Header = (props) => {
@@ -40,11 +40,28 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      
-      <p>Number of exercises {props.part[0].exercises + props.part[1].exercises + props.part[2].exercises}</p> 
+
+      <p>Number of exercises {props.part[0].exercises + props.part[1].exercises + props.part[2].exercises}</p>
     </div>
   )
 }
+
+
+const Display = (props) => {
+
+
+  return (
+    <div>
+      
+      {props.text}
+      {props.value}
+      {props.percent}
+    </div>
+  )
+}
+
+
+
 
 
 const App = () => {
@@ -66,11 +83,50 @@ const App = () => {
     ]
   }
 
+
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState(0)
+
+  const goodClicks = () => {
+    setAll(allClicks+1)
+    setGood(good + 1)
+  }
+
+  const neutralClicks = () => {
+    setAll(allClicks+1)
+    setNeutral(neutral + 1)
+  }
+
+  const badClicks = () => {
+    setAll(allClicks+1)
+    setBad(bad + 1)
+  }
+
   return (
     <div>
+
       <Header course={course.name} />
       <Content part={course.parts} />
       <Total part={course.parts} />
+
+      <button onClick={goodClicks}> good</button>
+
+      <button onClick={neutralClicks}> neutral</button>
+
+      <button onClick={badClicks}> bad</button>
+
+     
+      <Display text={"Good: "} value={good} />
+      <Display text={"Neutral: "} value={neutral} />
+      <Display text={"Bad: "} value={bad} />
+      <Display text={"All: "} value={allClicks} />
+      <Display text={"Average: "} value={(good-bad)/allClicks} />
+      <Display text={"Positive: "} value={(good*100)/allClicks} percent={"%"} />
+
+
     </div>
   )
 }
